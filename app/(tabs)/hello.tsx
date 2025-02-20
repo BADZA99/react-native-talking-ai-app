674a5468c163e4f45bfd414de420b55be1ccd060
papabn <pndiaye999@gmail.com>
2025-02-20 03:08:21 +0000

    setup and first commit

---
+++ app/(tabs)/hello.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+
+export default function hello() {
+  return (
+    <View>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: "center",
+            marginTop: 32,
+            marginBottom: 32,
+            color: "white",
+          }}
+        >Hellohhhhhhhhhhhhhh</Text>
+      
+    </View>
+  )
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+//npx create-expo-app@latest nom puis npx expo start --port 19001
+//le dossier tabs est le dossier de l'onglet du menu 
+//le dossier app est le dossier de l'application
+//chaque tab doit correspondre au nom de la page
+//chaque page doit etre dans le dossier tabs
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome! papa bn </ThemedText>
+        <HelloWave />
+      </ThemedView>
+     
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
